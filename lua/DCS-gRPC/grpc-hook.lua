be132a7a8e99d83c444e80261c40ee76c827cda9
MartinCo <martin@hatchlane.com>
2023-05-06 19:33:12 +0100

    Include slot type too

--- lua/DCS-gRPC/grpc-hook.lua
+++ lua/DCS-gRPC/grpc-hook.lua
@@ -85,12 +85,13 @@ end
 
 function handler.onPlayerChangeSlot(playerId)
   local playerInfo = net.get_player_info(playerId)
-  local coalition, slot, slotName
+  local coalition, slot, slotName, slotType
 
   if playerInfo ~= nil then
     coalition = playerInfo.side + 1 -- offsetting for grpc COALITION enum
     slot = playerInfo.slot
-    slotName = DCS.getUnitProperty(playerInfo.slot, DCS.UNIT_GROUPNAME) or playerInfo.slot
+    slotName = DCS.getUnitProperty(playerInfo.slot, DCS.UNIT_GROUPNAME)
+    slotType = DCS.getUnitProperty(playerInfo.slot, DCS.UNIT_TYPE)
   end
 
   grpc.event({
@@ -101,6 +102,7 @@ function handler.onPlayerChangeSlot(playerId)
       coalition = coalition,
       slotId = slot,
       slotName = slotName,
+      slotType = slotType,
     },
   })
 end
