724ccd247f813ff87bb7d5d94d142ccaad77c6c6
DastishFantastic <karus2014@gmail.com>
2026-07-14 20:11:25 +0300

    Build.bat output to source

--- build.bat
+++ build.bat
@@ -1,1 +1,26 @@
-cargo build && copy /Y target\debug\dcs_grpc.dll "C:\Users\Martin\Saved Games\DCS.server\Mods\tech\DCS-gRPC\dcs_grpc.dll" && xcopy /SY lua\DCS-gRPC "C:\Users\Martin\Saved Games\DCS.server\Scripts\DCS-gRPC"
+@echo off
+setlocal
+cd /d "%~dp0"
+
+cargo build --release
+if errorlevel 1 exit /b 1
+
+set OUT=%~dp0sdcs-grpc
+
+if exist "%OUT%" rmdir /S /Q "%OUT%"
+mkdir "%OUT%\Mods\tech\DCS-gRPC"
+mkdir "%OUT%\Scripts\DCS-gRPC"
+mkdir "%OUT%\Scripts\Hooks"
+
+copy /Y "target\release\dcs_grpc.dll" "%OUT%\Mods\tech\DCS-gRPC\dcs_grpc.dll"
+if errorlevel 1 exit /b 1
+
+xcopy /YSQ "lua\DCS-gRPC\*" "%OUT%\Scripts\DCS-gRPC\"
+if errorlevel 1 exit /b 1
+
+xcopy /YSQ "lua\Hooks\*" "%OUT%\Scripts\Hooks\"
+if errorlevel 1 exit /b 1
+
+echo.
+echo Packaged to: %OUT%
+endlocal
